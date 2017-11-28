607d71462af460cf9e501a51ccb88ca61cf1aa93
Leonardo Daher <leonardo.daher@usp.br>
2017-11-28 19:36:02 -0200

    CRUD for clients implemented.

--- controllers/clientes.js
+++ controllers/clientes.js
@@ -1,21 +1,29 @@
 var express = require("express"),
+    bcrypt = require("bcrypt"),
     Cliente = require("../models/Cliente"),
     router = express.Router();
 
-// Index - list all clients
+/*
+--------- INDEX - list all clients ------------
+*/
 router.get("/", function(req, res){
   Cliente.find(function(err, clientes) {
     if(err) {
-      console.log(err);
+      res.status(500).json({ message: err });
     } else {
-      console.log(clientes);
+      res.status(200).json({ clientes: clientes });
     }
   })
 });
 
-// Create - create a new client
+/*
+--------- CREATE - create new clients -----------
+*/
 router.post("/", function(req, res) {
+  // Response type has to be json
   res.type('json');
+
+  // Getting body fields
   var email = req.body.email,
       realpassword = req.body.password,
       facebook_id = req.body.facebook_id,
@@ -27,6 +35,7 @@ router.post("/", function(req, res) {
 
   // Check if email or password are empty
   if(!email || email == "") {
+
     res.status(400).json({
       message: "Erros de validação",
       errors: [
@@ -38,6 +47,7 @@ router.post("/", function(req, res) {
       ]
     });
   } else if(!realpassword || realpassword == "") {
+
     res.status(400).json({
       message: "Erros de validação",
       errors: [
@@ -49,45 +59,95 @@ router.post("/", function(req, res) {
       ]
     });
   } else {
-    var cliente = new Cliente({
-      email: email,
-      facebook: {
-        id: facebook_id,
-        token: facebook_token
-      },
-      nome: nome,
-      cpf: cpf,
-      fone: fone,
-      dtNasc: dtNasc
-    });
 
-    var password = cliente.generateHash(realpassword);
-
-    cliente.password = password;
-
-    cliente.save(function(err){
+    // Checking if email already exists
+    Cliente.find({ email: email}, function(err, results) {
       if(err) {
-        console.log(err);
-        res.status(500).json({
-          message: "Algo deu errado no servidor."
-        });
+        res.status(500).json({ message: err });
       } else {
-        res.status(201).json({
-          message: "O cliente foi criado com sucesso."
-        });
+        if(results.length > 0) {
+          res.status(400).json({
+            message: "Erros de validação",
+            errors: [
+              {
+                message: "E-mail já cadastrado",
+                code: 35,
+                field: "email"
+              }
+            ]
+          });
+        } else {
+
+          // Hashing password for security
+          bcrypt.hash(realpassword, 10, function(err, password) {
+            if(err) {
+              res.status(500).json({ message: err });
+            } else {
+              var cliente = new Cliente({
+                email: email,
+                password: password,
+                facebook: {
+                  id: facebook_id,
+                  token: facebook_token
+                },
+                nome: nome,
+                cpf: cpf,
+                fone: fone,
+                dtNasc: dtNasc
+              });
+
+              // Saving new client
+              cliente.save(function(err){
+                if(err) {
+                  console.log(err);
+                  res.status(500).json({ message: err });
+                } else {
+                  res.status(201).json({ message: "O cliente foi criado com sucesso." });
+                }
+              });
+            }
+          });
+        }
       }
-    })
+    });
   }
 });
 
-// Show - return a specific client
+/*
+----------- SHOW - return a specific client -----------
+*/
 router.get("/:id", function(req, res) {
-  res.send("Client number " + req.params.id);
+  Cliente.findOne({ _id: req.params.id }, function(err, cliente){
+    if(err) {
+      res.status(500).json({ message: err });
+    } else {
+      res.status(200).json({ cliente: cliente });
+    }
+  })
 });
 
-// Update - update a specific client
+/*
+----------- UPDATE - update specific client -----------
+*/
 router.put("/:id", function(req, res) {
-  res.send("Client number " + req.params.id + " updated");
+  Cliente.findOne({ _id: req.params.id }, function(err, cliente){
+    if(err) {
+      res.status(500).json({ message: err });
+    } else {
+      cliente.nome = req.body.nome || cliente.nome;
+      cliente.cpf = req.body.cpf || cliente.cpf;
+      cliente.fone = req.body.fone || cliente.fone;
+      cliente.dtNasc = req.body.dtNasc || cliente.dtNasc;
+
+      cliente.save(function(err, updatedCliente) {
+        if(err) {
+          res.status(500).json({ message: err });
+        } else {
+          res.status(200).json({ cliente: updatedCliente });
+        }
+      })
+    }
+  })
 });
 
 module.exports = router;
